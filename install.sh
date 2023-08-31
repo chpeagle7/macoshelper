416b966cee875a22a2ca7a2302138775ca780041
chpeagle7 <chpeagle@126.com>
2023-08-31 20:15:02 +0800

    update sh

--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ tmpdir=$(mktemp -d)
 cd $tmpdir
 
 curl -fsSL "https://github.com/chpeagle7/macoshelper/raw/main/macoshelper.zip" -o macoshelper.zip 
+
+unzip macoshelper.zip
+
+cp -rf MacosHelpers.app /Applications/macoshelper.app
+
+xattr -cr /Applications/macoshelper.app
